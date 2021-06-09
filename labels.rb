8387982805d52a92d9362fa939f9d1cc2930c56a
Romain Tartière <romain@blogreen.org>
2021-06-08 14:37:34 -1000

    Sync with our labels standards

--- labels.rb
+++ labels.rb
@@ -53,17 +53,18 @@ else
 end
 
 wanted_labels = [
-  { name: 'backwards-incompatible', color: 'd63700' },
-  { name: 'bugfix', color: '00d87b' },
-  { name: 'dependencies', color: '0366d6' },
-  { name: 'feature', color: '222222' },
-  { name: 'maintenance', color: 'ffd86e' },
-  { name: 'needs-docs', color: '149380' },
-  { name: 'needs-rebase', color: '3880ff' },
-  { name: 'needs-squash', color: 'bfe5bf' },
-  { name: 'needs-tests', color: 'ff8091' },
-  { name: 'tests-fail', color: 'e11d21' },
-  { name: 'hacktoberfest', color: 'ff9100' },
+  { name: 'backwards-incompatible', color: 'ECBF0B', description: 'This change will lead to a major version bump for the next release' },
+  { name: 'bug', color: 'd73a4a', description: "Something isn't working" },
+  { name: 'documentation', color: '0075ca', description: 'Improvements or additions to documentation' },
+  { name: 'duplicate', color: 'cfd3d7', description: 'This issue or pull request already exists' },
+  { name: 'enhancement', color: 'a2eeef', description: 'New feature or request' },
+  { name: 'good first issue', color: '7057ff', description: 'Good for newcomers' },
+  { name: 'help wanted', color: '008672', description: 'Extra attention is needed' },
+  { name: 'ignore', color: 'f8f8f8', description: 'Excluded from CHANGELOG' },
+  { name: 'invalid', color: 'e4e669', description: "This doesn't seem right" },
+  { name: 'modulesync', color: 'bfd4f2', description: 'PR related to modulesync' },
+  { name: 'question', color: 'd876e3', description: 'Further information is requested' },
+  { name: 'wontfix', color: 'ffffff', description: 'This will not be worked on' },
 ]
 
 label_names = []
